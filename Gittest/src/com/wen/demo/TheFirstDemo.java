f7a00e4936b9618c4ad29130aab5062a1fc50765
wen <76898808@qq.com>
2018-01-25 09:14:44 +0800

    添加gittest项目到本地仓库

---
+++ Gittest/src/com/wen/demo/TheFirstDemo.java
@@ -0,0 +1,28 @@
+package com.wen.demo;
+/**
+ * @description TODO
+ * @author {wen yong}
+ * @date 2018年1月24日 下午9:40:53 
+ * @update 2018年1月24日 下午9:40:53 
+ * @version 
+ *
+ * 
+ */
+public class TheFirstDemo {
+	public static void main(String[] args) {
+		
+	}
+	/**
+	 * 说的方法
+	 */
+	public void say() {
+		System.out.println("say hello Git");
+	}
+	
+	/**
+	 * 移动的方法
+	 */
+	public void move() {
+		System.out.println("新增一个说话的方法");
+	}
+}
